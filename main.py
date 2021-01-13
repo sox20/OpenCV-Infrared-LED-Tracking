2fffe06acae27a6d4dbdad38d6cc3a455172a17b
sox20 <55483396+sox20@users.noreply.github.com>
2021-01-13 20:12:27 +1100

    simple contour based tracking

--- main.py
+++ main.py
@@ -2,19 +2,39 @@ import cv2 as cv
 import numpy as np
 
 cap = cv.VideoCapture(0)
-cap.set(3, 480)
-cap.set(4, 270)
+#cap.set(3, 480)
+#cap.set(4, 270)
 
 while(True):
     # Capture frame by frame
     ret, frame = cap.read()
-
-    # converts image to grayscale and applies binary threshold
+    
     gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
-    ret, thres = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-
+    
+    mask = cv.inRange(gray, 240, 255)
+    mask = cv.blur(mask, (6,6))
+    
+    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[-2:]
+    
+    tracked_contours = []
+    
+    for i in contours:
+        x, y, w, h = cv.boundingRect(i)
+        # checks min and max area and checks how "sqaure" the bounding box
+        if cv.contourArea(i) <= 3000.0 and cv.contourArea(i) > 150 and w/h < 1.2 and w/h > 0.8:
+            tracked_contours.append(i)
+            
+    for i in tracked_contours:
+        x, y, w, h = cv.boundingRect(i)
+        
+        # render bounding box
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
+        # render circle at center of bounding box
+        cv.circle(frame, (x + int(w/2), y + int(h/2)), 4, (255, 0, 0), -1)
+    
     # Display the resulting frame
-    cv.imshow('frame', thres)
+    cv.imshow('frame', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
